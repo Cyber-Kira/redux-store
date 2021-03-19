2125b22a02c0e9c02fb14e804fc6009e779518b1
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-03-19 20:12:44 +0300

    return Promise in order to simulate real api workflow

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,20 +1,26 @@
 export default class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: "Production-Ready Microservices",
+      author: "Susan J. Fowler",
+      price: 34,
+      coverImage: "/Production-Ready.jpg",
+    },
+    {
+      id: 2,
+      title: "release It!",
+      author: "Michael T. Nygard",
+      price: 45,
+      coverImage: "/Release-It.jpg",
+    },
+  ];
+
   getBooks() {
-    return [
-      {
-        id: 1,
-        title: "Production-Ready Microservices",
-        author: "Susan J. Fowler",
-        price: 34,
-        coverImage: "/Production-Ready.jpg",
-      },
-      {
-        id: 2,
-        title: "release It!",
-        author: "Michael T. Nygard",
-        price: 45,
-        coverImage: "/Release-It.jpg",
-      },
-    ];
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.data);
+      }, 700);
+    });
   }
 }
