c6a7b292614f525ed15708c6bc34ade74fb92dd2
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-03-20 21:07:29 +0300

    use new action to add event listener on button item

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -4,18 +4,21 @@ import { connect } from "react-redux";
 import compose from "../../utils/compose";
 import ErrorIndicator from "../error-indicator";
 
-import { fetchBooks } from "../../actions";
+import { fetchBooks, bookAddedToCart } from "../../actions";
 import { withBookstoreService } from "../hoc";
 import Spinner from "../spinner";
 import "./book-list.css";
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
   return (
     <ul className="book-list">
       {books.map((book) => {
         return (
           <li key={book.id}>
-            <BookListItem book={book} />
+            <BookListItem
+              onAddedToCart={() => onAddedToCart(book.id)}
+              book={book}
+            />
           </li>
         );
       })}
@@ -28,7 +31,7 @@ class BookListContainer extends Component {
   }
 
   render() {
-    const { books, loading, error } = this.props;
+    const { books, loading, error, onAddedToCart } = this.props;
 
     if (loading) {
       return <Spinner />;
@@ -38,7 +41,7 @@ class BookListContainer extends Component {
       return <ErrorIndicator />;
     }
 
-    return <BookList books={books} />;
+    return <BookList onAddedToCart={onAddedToCart} books={books} />;
   }
 }
 
@@ -49,6 +52,7 @@ const mapStateToProps = ({ books, loading, error }) => {
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
   return {
     fetchBooks: fetchBooks(bookstoreService, dispatch),
+    onAddedToCart: (id) => dispatch(bookAddedToCart(id)),
   };
 };
 
