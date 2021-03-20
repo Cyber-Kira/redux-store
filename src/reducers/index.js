09118564db45465558431b19583d00a68868e708
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-03-20 20:51:32 +0300

    create shopping-cart-table component with hardcoded data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,33 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: "Book 1",
+      count: 3,
+      total: 150,
+    },
+    {
+      id: 2,
+      name: "Book 2",
+      count: 1,
+      total: 50,
+    },
+    {
+      id: 3,
+      name: "Book 3",
+      count: 3,
+      total: 70,
+    },
+    {
+      id: 4,
+      name: "Book 4",
+      count: 1,
+      total: 30,
+    },
+  ],
+  orderTotal: 300,
 };
 
 const reducer = (state = initialState, action) => {
@@ -13,12 +40,14 @@ const reducer = (state = initialState, action) => {
       };
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
